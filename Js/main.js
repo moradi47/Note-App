168c3713393fb23dd617d39e42ad57b953ad1c07
ms.moradi <ms.moradi47@gmail.com>
2024-12-01 14:59:44 +0330

    Defining the elements based on the class name and the definition of the colors object

---
+++ Js/main.js
@@ -0,0 +1,20 @@
+let inputElem  = document.querySelector('.name-work');
+let addBtnElem = document.querySelector('.add-btn');
+let divContainerElem = document.querySelector('.list-works');
+let eraseBtnElem = document.querySelector('.erase-btn');
+let backgroundColor
+
+let colors ={
+    white: '#fff',
+    orange: '#FFD07F',
+    yellow:  '#FCFB82',
+    paleGreen:  '#D3F97D',
+    green:  '#76F87F',
+    blueGreen:  '#7DFAD5',
+    paleBlue:  '#7EFBFF',
+    blue:  '#7FD2F9',
+    deepBlue:  '#7B83FB',
+    purple: '#D788FF',
+    pink: '#FF87FD',
+}
+
